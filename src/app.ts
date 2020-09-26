6fd78747a2d494abba10370136ab9e52fa9e4985
AiMrz <zoz.zozm@gmai.com>
2020-09-26 12:56:03 +0330

    add @types/ws

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 "use strict";
 import TradeBroker from "./server";
-const server: TradeBroker = new TradeBroker(process.env.API_PORT || 3000);
+const server: TradeBroker = new TradeBroker(process.env.SERVER_PORT || 3000);
 // starting the server
 server.startServer();
